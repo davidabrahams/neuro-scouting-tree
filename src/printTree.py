9bba80971bf72243570a2ae4cf461ea857a38a5a
David Abrahams <dabrahams@dabrahams-Latitude-E6440.(none)>
2014-10-28 20:09:59 -0400

    An initial script that simply prints the script's input argument

--- src/printTree.py
+++ src/printTree.py
@@ -2,9 +2,3 @@ import sys
 
 lines = sys.argv[1]
 print(lines)  #print the user input
-
-
-def generateLine(prevLine):
-    thisLine = 2 * len(prevLine) * [0]  #initialize
-    for i in range(len(prevLine)):
-        parent = 0
