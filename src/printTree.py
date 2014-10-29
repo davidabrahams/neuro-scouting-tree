a322315ea14448c8a12b808e50fe16a39d1b2fdb
David Abrahams <dabrahams@dabrahams-Latitude-E6440.(none)>
2014-10-29 00:33:59 -0400

    added treeBuilder module with a function that computes the next layer of a tree based on the previous layer

--- src/printTree.py
+++ src/printTree.py
@@ -1,4 +1,16 @@
 import sys
 
-lines = sys.argv[1]
-print(lines)  #print the user input
+#read the number of layers the tree will have from the input argument
+layers = int(sys.argv[1])
+
+#initialize the tree and set the starting node
+tree = [None] * layers
+if layers > 0:
+    tree[0] = [1]
+
+#generate the tree
+for i in range(1, layers):
+    tree[i] = [1] * (2 * len(tree[i - 1]))
+
+#print the tree
+print(tree)
--- src/treeBuilder.py
+++ src/treeBuilder.py
@@ -1,15 +1,37 @@
-def buildnextline(prevline):
-    nextLine = [0] * (2 * len(prevLine))
-    for i in range(len(nextLine)):
+def next_line(prev_layer):
+    """
+    @param prev_layer: the previous line in the tree
+    @return: next_layer: the next layer in the tree, computed based on the previous layer
+    """
+
+    #initialize the return variable
+    next_layer = [0] * (2 * len(prev_layer))
+
+    #calculate the values of all the nodes in the next layer
+    for i in range(len(next_layer)):
         #calculate the index of the parent
-        parentIndex = i / 2
+        parent_index = i / 2
 
         #if the node is at an even index then it is a left child
-        isNodeLeftSib = (i % 2 == 0)
+        node_left = (i % 2 == 0)
 
         #calculate the index of the respective sibling of the parent
-        if isNodeLeftSib:
-            parentSiblingIndex = parentIndex - 1
+        if node_left:
+            parent_sib_index = parent_index - 1
         else:
-        parentSiblingIndex = parentIndex + 1
-    return 0
+            parent_sib_index = parent_index + 1
+
+        #calculate the new node in the tree
+        node = prev_layer[parent_index]
+        if (parent_sib_index > 0) and (parent_sib_index < len(prev_layer)):
+            node += prev_layer[parent_sib_index]
+
+        #place the node in the next layer
+        next_layer[i] = node
+
+    return next_layer
+
+#test the next_line function, output should be [1, 3, 2, 4, 4, 3, 3, 1]
+test_layer = [1, 2, 2, 1]
+next_layer = next_line(test_layer)
+print next_layer
